024fbe113332b54aaf252957fc30c416744b5938
Ennovasoft <user@macbook-pro-de-user.local>
2018-07-12 13:43:32 -0500

    0.0.3 JASB Revisión de Cambios

--- FirstProjectEnnovasoft/FirstProjectEnnovasoft/ViewController.swift
+++ FirstProjectEnnovasoft/FirstProjectEnnovasoft/ViewController.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2018 Ennovasoft. All rights reserved.
 //  comentario de prueba
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cadena : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
